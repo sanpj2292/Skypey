2d7cc93c762273492def0e46a610b8857a28b0a4
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-23 16:25:32 +0530

    setting proper user_id on clicking respective contacts to ChatWindow & understanding combineReducers effectively

--- src/containers/user.js
+++ src/containers/user.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./user.css";
 
+import store from "../store";
+import { setActiveUserId } from "../actions";
+
+function handleUserClick({ user_id }) {
+    console.log(user_id);
+    store.dispatch(setActiveUserId(user_id))
+};
+
 const User = ({ user }) => {
     const { name, profile_pic, status } = user;
 
     return (
-        <div className="User">
+        <div className="User" onClick={handleUserClick.bind(null, user)}>
             <img src={profile_pic} alt={name} className="User__pic" />
             <div className="User__details">
                 <p className="User__details-name">{name}</p>
